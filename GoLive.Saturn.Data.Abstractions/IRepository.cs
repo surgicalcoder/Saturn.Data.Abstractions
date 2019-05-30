a1442e530afee939024ed65856f2bb6f9250b031
Monty <ma@surgicalcoder.com>
2019-05-30 22:26:23 +0100

    Added in paging

--- GoLive.Saturn.Data.Abstractions/IRepository.cs
+++ GoLive.Saturn.Data.Abstractions/IRepository.cs
@@ -9,6 +9,8 @@ namespace GoLive.Saturn.Data.Abstractions
 {
     public interface IRepository : IDisposable
     {
+        void DisposeConnection();
+        Task DeleteMany<T>(List<string> IDs, string overrideCollectionName = "") where T : Entity;
         Task<T> ById<T>(string id, string overrideCollectionName = "") where T : Entity;
         Task<List<T>> ById<T>(List<string> IDs, string overrideCollectionName = "") where T : Entity;
 
@@ -21,7 +23,12 @@ namespace GoLive.Saturn.Data.Abstractions
         Task<IQueryable<T>> All<T>(string overrideCollectionName = "") where T : Entity;
         Task<T> One<T>(Expression<Func<T, bool>> predicate, string overrideCollectionName = "") where T : Entity;
         Task<IQueryable<T>> Many<T>(Expression<Func<T, bool>> predicate, string overrideCollectionName = "") where T : Entity;
+        Task<List<T>> Many<T>(Dictionary<string, object> WhereClause, string overrideCollectionName = "") where T : Entity;
 
+        Task<IQueryable<T>> Many<T>(Expression<Func<T, bool>> predicate, int pageSize, int PageNumber, string overrideCollectionName = "") where T : Entity;
+        Task<List<T>> Many<T>(Dictionary<string, object> WhereClause, int pageSize, int PageNumber, string overrideCollectionName = "") where T : Entity;
+
+        Task<long> CountMany<T>(Expression<Func<T, bool>> predicate, string overrideCollectionName = "") where T : Entity;
         Task Add<T>(T entity, string overrideCollectionName = "") where T : Entity;
         Task AddMany<T>(IEnumerable<T> entities, string overrideCollectionName = "") where T : Entity;
 
@@ -33,18 +40,10 @@ namespace GoLive.Saturn.Data.Abstractions
         Task UpsertMany<T>(List<T> entity, string overrideCollectionName = "") where T : Entity;
 
         Task Delete<T>(T entity, string overrideCollectionName = "") where T : Entity;
+        Task Delete<T>(Expression<Func<T, bool>> filter, string overrideCollectionName = "") where T : Entity;
         Task Delete<T>(string Id, string overrideCollectionName = "") where T : Entity;
         Task DeleteMany<T>(IEnumerable<T> entity, string overrideCollectionName = "") where T : Entity;
-        Task DeleteMany<T>(List<string> IDs, string overrideCollectionName = "") where T : Entity;
 
-        void DisposeConnection();
         void InitDatabase();
-
-
-        Task Delete<T>(Expression<Func<T, bool>> filter, string overrideCollectionName = "") where T : Entity;
-
-
-        Task<List<T>> Many<T>(Dictionary<string, object> WhereClause, string overrideCollectionName = "") where T : Entity;
-        Task<long> CountMany<T>(Expression<Func<T, bool>> predicate, string overrideCollectionName = "") where T : Entity;
     }
 }
